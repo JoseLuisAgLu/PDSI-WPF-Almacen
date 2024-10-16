7d0602e5724c1ddcd981724a5a2e295346b04261
4lebandr0 <a219206862@unison.mx>
2024-10-16 14:46:35 -0700

    Added ProductBD.cs & PruebasProductoBD.cs

---
+++ Rider-Almacen/Unison-Almacen/Unison-Almacen-Core/BaseDeDatos/ProductBD.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Unison_Almacen_Core.Modelos;
+
+namespace Unison_Almacen_Core.BaseDeDatos;
+
+public class ProductBD : DbContext
+{
+    private const string NombreBaseDeDatos = "datos.db";
+    
+    /// <summary>
+    /// Configura la direccion de la base de datos.
+    /// </summary>
+    public DbSet<Producto> Productos
+    {
+        get;
+        set;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var directorio = AppContext.BaseDirectory;
+        // 1.   Asegurar que existe el directorio de la base de datos.
+        if (!Directory.Exists(directorio)) Directory.CreateDirectory(directorio);
+        // 2.   Configuramos el nombre de la base de datos.
+        optionsBuilder.UseSqlite($"Filename={directorio}/{NombreBaseDeDatos}",
+            op => op.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName));
+        // 3.   Terminar con la configuracion
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Definir la tabla producto
+        modelBuilder.Entity<Producto>(producto =>
+        {
+            producto.ToTable(nameof(Productos));
+            producto.HasKey(p => p.Id);
+            producto.Property(p => p.Nombre).IsRequired();
+            producto.Property(p => p.Descripcion).IsRequired();
+            producto.Property(p => p.Precio).IsRequired();
+        });
+    }
+    
+}
---
+++ Rider-Almacen/Unison-Almacen/Unison-Almacen-Test/BaseDeDatos/PruebasProductoBD.cs
@@ -0,0 +1,49 @@
+﻿using Unison_Almacen_Core.BaseDeDatos;
+using Unison_Almacen_Core.Modelos;
+
+namespace Unison_Almacen_Test.BaseDeDatos;
+
+public class PruebasProductoBD
+{
+    
+    private ProductBD _productoDB;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _productoDB = new ProductBD();          // 1.   Crea una instancia de la base de datos.
+        _productoDB.Database.EnsureCreated();   // 2.   Se asegura que existe la base de datos.
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _productoDB.Database.EnsureDeleted();   // 1.   Elimina la base de datos.
+        _productoDB.Dispose();                  // 2.   Libera los recursos de la conexion.
+    }
+
+    [Test]
+    public void PruebaAgregarProducto()
+    {
+        // 1.   Crear el Id del producto
+        var id = Guid.NewGuid();
+        
+        // 2.   Crear un producto
+        Producto producto = new()
+        {
+            Id = id,
+            Nombre = "Producto 1",
+            Descripcion = "Descripcion 1",
+            Precio = 500f
+        };
+        // 3.   A;adir el producto a la base de datos.
+        _productoDB.Productos.Add(producto);
+        _productoDB.SaveChanges();
+        
+        // 4.   Consultar los productos para comprobar que se a;adio el producto
+        var resultado = _productoDB.Productos.Find(id);
+        
+        Assert.That(resultado, Is.Not.Null, "No se agrego el producto.");
+        Assert.That(resultado.Id, Is.EqualTo(id));
+    }
+}
