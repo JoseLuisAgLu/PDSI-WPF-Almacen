04e291b15e1ccce0101e8e414ed75a2399c06100
Jose Luis Aguilera Luzania <167374728+JoseLuisAgLu@users.noreply.github.com>
2024-10-22 22:33:17 -0700

    + Se definió el contenido de inicio.
    Se modifico la vista de la ventana principal para mostrar la vista InicioView como contenido principal, además de agrego una vista para mostrar los productos llamada ProductoView.

--- Rider-Almacen/Unison-Almacen/Unison-Almacen-App/MainWindow.xaml.cs
+++ Rider-Almacen/Unison-Almacen/Unison-Almacen-App/MainWindow.xaml.cs
@@ -1,31 +1,20 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Unison_Almacen_Core.Contratos.Repositorios;
-using Unison_Almacen_Core.Contratos.Servicios;
-using Unison_Almacen_Core.Modelos;
-using Unison_Almacen_Core.Servicios;
+﻿using Unison_Almacen_App.Views;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
 namespace Unison_Almacen_App;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : FluentWindow
 {
-    
     public MainWindow()
     {
-        //InitializeComponent();
+        // Inicializa el tema de la aplicación.
         ApplicationThemeManager.Apply(this);
+
+        // Establece la página de inicio.
+        Loaded += (_, _) => RootNavigation.Navigate(typeof(InicioView));
     }
 }
---
+++ Rider-Almacen/Unison-Almacen/Unison-Almacen-App/Views/ProductoView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Unison_Almacen_App.Views;
+
+public partial class ProductoView : Page
+{
+    public ProductoView()
+    {
+        InitializeComponent();
+    }
+}
