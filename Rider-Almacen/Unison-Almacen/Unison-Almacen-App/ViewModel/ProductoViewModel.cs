451b82dc126695b17a104f1c7b944d322e5017fc
Jose Luis Aguilera Luzania <167374728+JoseLuisAgLu@users.noreply.github.com>
2024-11-04 15:21:51 -0700

    + Se agregó una DataGrid para mostrar el contenido de la tabla Productos en ProductoView.xaml. + Se agregaron los servicios para obtener los datos de los productos en el ProductoViewModel.cs. * Se modificaron las funciones en el ProductoViewModel.cs para agregar productos a la tabla en la base de datos.

--- Rider-Almacen/Unison-Almacen/Unison-Almacen-App/ViewModel/ProductoViewModel.cs
+++ Rider-Almacen/Unison-Almacen/Unison-Almacen-App/ViewModel/ProductoViewModel.cs
@@ -1,23 +1,48 @@
 ﻿using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Unison_Almacen_Core.Contratos.Servicios;
 using Unison_Almacen_Core.Modelos;
+using Unison_Almacen_Core.Servicios;
 
 namespace Unison_Almacen_App.ViewModel;
 
 public partial class ProductoViewModel : ObservableObject
 {
-    [ObservableProperty] private Producto producto = new Producto();
+    [ObservableProperty] private Producto _producto = new Producto();
+    [ObservableProperty] private List<Producto> _productos;
+    
+    private IServicio<Producto> _servicio;
 
-    public ProductoViewModel()
+    public ProductoViewModel(IServicio<Producto> servicio)
     {
+        // Definir el comando.
         AgregarProductoCommand = new RelayCommand(AgregarProducto);
+        
+        // Guardar la referencia del servicio.
+        _servicio = servicio;
+        
+        // Obtener los productos de la base de datos.
+        _productos = _servicio.Listar();
     }
     
     public ICommand AgregarProductoCommand { get; }
 
     private void AgregarProducto()
     {
-        Producto p = new Producto();
+        var p = Producto;
+        if (string.IsNullOrWhiteSpace(p.Nombre) || string.IsNullOrWhiteSpace(p.Descripcion)) return;
+        
+        // Agregar el producto.
+        _servicio.Agregar(p);
+        
+        // Actualizamos la lista de productos.
+        Productos = _servicio.Listar();
+            
+        // Borramos los datos del formulario.
+        Producto.Nombre = string.Empty;
+        Producto.Descripcion = string.Empty;
+        
+        // Actualizar la tabla.
     }
 }
